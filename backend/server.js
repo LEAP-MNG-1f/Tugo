04ab380e638cf175c42bc3f1746152e94d55ac7b
Hustle999 <tuguutuugii02@gmail.com>
2024-11-06 12:38:44 +0800

    back front holboson

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,61 @@
 // })
 
 // server.listen(6969)
+import bodyParser from "body-parser";
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
 
-const port= 6969
-const app = express()
+const port = 6969;
+const app = express();
 
-app.listen(port,()=>{
-console.log("ajillav")
-})
+app.use(bodyParser.json());
+app.use(cors());
+
+const users = [
+  {
+    id: 1,
+    name: "tugu",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "MB",
+    age: 32,
+  },
+];
+
+app.get("/users", (request, response) => {
+  response.send(users);
+});
+
+// app.post("/users", (request, response) => {
+//   // const userName = request.body.name;
+//   // const userAge = request.body.Age;
+//   const idNumber = Date.now().toString();
+//   const { name, age } = request.body;
+
+//   const newUser = {
+//     id: idNumber,
+//     name: name,
+//     age: age,
+//   };
+
+//   users.push(newUser);
+//   response.send(users);
+// });
+
+app.put("/users", (request, response) => {
+  console.log(request.body);
+  response.send(users);
+});
+
+app.delete("/users", (request, response) => {
+  console.log(request.body);
+  response.send(users);
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
